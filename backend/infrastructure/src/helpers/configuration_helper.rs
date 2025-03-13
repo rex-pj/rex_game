8f9713c0a22f2bf19d6859bd0154cb838c06d1b3
Rex <delta@hotmail.com.vn>
2025-03-14 01:43:51 +0700

    Config the environment

--- backend/infrastructure/src/helpers/configuration_helper.rs
+++ backend/infrastructure/src/helpers/configuration_helper.rs
@@ -1,12 +1,47 @@
+use std::env;
+
 use config::{Config, File};
 use rex_game_domain::helpers::configuration_helper_trait::ConfigurationHelperTrait;
 
 #[derive(Clone)]
-pub struct ConfigurationHelper;
+pub struct ConfigurationHelper {
+    _config: Config,
+}
 
 impl ConfigurationHelper {
+    fn get_host() -> String {
+        let args: Vec<String> = env::args().collect();
+        if args.len() == 0 {
+            return String::from("");
+        }
+
+        match args.iter().find(|&x| String::from(x).contains("host=")) {
+            Some(value) => {
+                let stripped_value = value.strip_prefix("host=").unwrap_or("");
+                let host_value = ".".to_owned() + stripped_value;
+
+                return host_value;
+            }
+            None => String::from(""),
+        }
+    }
+
+    fn get_config() -> Config {
+        let host = Self::get_host();
+        let config_path = "src/config".to_owned() + host.as_str() + ".toml";
+        let config_file = File::with_name(config_path.as_str());
+        let settings = Config::builder()
+            .add_source(config_file)
+            .build()
+            .expect("Failed to load configuration");
+
+        settings
+    }
+
     pub fn new() -> Self {
-        Self
+        Self {
+            _config: Self::get_config(),
+        }
     }
 
     pub fn get_value(&self, key: &str) -> String {
@@ -16,13 +51,7 @@ impl ConfigurationHelper {
 
 impl ConfigurationHelperTrait for ConfigurationHelper {
     fn get_value(&self, key: &str) -> String {
-        let config_file = File::with_name("src/config.toml");
-        let settings = Config::builder()
-            .add_source(config_file)
-            .build()
-            .expect("Failed to load configuration");
-
         let error_message = key.to_owned() + "'s value is missing";
-        settings.get_string(key).expect(error_message.as_str())
+        self._config.get_string(key).expect(error_message.as_str())
     }
 }
